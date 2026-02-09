0eb3a9ff84a58c212d0477e76d7cb33cff9bebec
Kevin Stephens <kevinroncolato@outlook.com>
2026-02-09 12:43:50 +0000

    apache common to pom and screenshots

--- cicd/CICD/src/test/java/com/tus/cicd/GoogleSearchIT.java
+++ cicd/CICD/src/test/java/com/tus/cicd/GoogleSearchIT.java
@@ -7,8 +7,20 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class GoogleSearchIT {
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
+
+
+public class GoogleSearchIT {
+	
+	
+	
+	
     @Test
     void verifyGoogleHomePageLoads() {
         ChromeOptions options = new ChromeOptions();
@@ -18,6 +30,15 @@ public class GoogleSearchIT {
         driver.get("https://www.google.com");
 
         assertTrue(driver.getTitle().contains("Google"));
+        
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        
+    	try {
+			FileUtils.copyFile(screenshot, new File("target/screenshots/failure.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         driver.quit();
     }
